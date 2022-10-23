cf768ec503770db9d4d7b1adf90c83a82ba44a50
Callisthenes <91435423+Callisthenes@users.noreply.github.com>
2022-10-23 21:35:21 +0200

    queries, aggregate queries and nifi template

---
+++ query_scripts/johnlegend.py
@@ -0,0 +1,53 @@
+import tweepy
+import configparser
+import pandas as pd
+import sys
+
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Stream
+from tweepy.streaming import Stream
+from time import sleep
+from random import randint
+
+
+# API keys corresponding to an academic account
+api_key = "4OxfMXFZL9yBDYboGSfcjbFsc"
+api_secrets = "Ny9LS2gPD7plQy48c3EpQ3UDvVfqrwvJyZLrAJlgEhQtWz3krW"
+access_token =  "1581220096529375238-2Kt6eULOqBD7EvtpqTf4gvU7n69MZ3"
+access_secret = "7irLEy7IiGfW72gsh895wntdk3n94upoyXtUMGSo98mch"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAPEbiQEAAAAAuuFvB9xcB%2FR4J2RXWNApbAWQ3PY%3DVqvJvvr5WlYDYSfMq5GcZqGGu2gapcfF0ezvJzCWfDaodqSE4L"
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(api_key, api_secrets)
+auth.set_access_token(access_token, access_secret)
+ 
+api = tweepy.API(auth)
+ 
+try:
+    api.verify_credentials()
+    #print('Successful Authentication')
+    #commented so it doesn't appear in hdfs
+except:
+    print('Failed authentication')
+    
+client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAPEbiQEAAAAAuuFvB9xcB%2FR4J2RXWNApbAWQ3PY%3DVqvJvvr5WlYDYSfMq5GcZqGGu2gapcfF0ezvJzCWfDaodqSE4L')
+
+# Get tweets that contain the hashtag #badbunny 
+# With location is US
+# -is:retweet means I don't want retweets
+# lang:en is asking for the tweets to be in english
+#maximum results are 100, modify this accroding to your api limit!!!!
+query = 'johnlegend -is:retweet lang:en place_country: US'
+# Replace with time period of your choice
+start_time = '2022-01-01T00:00:00Z'
+
+# Replace with time period of your choice
+end_time = '2022-10-01T00:00:00Z'
+# Replace the limit=1000 with the maximum number of Tweets you want
+for tweet in tweepy.Paginator(client.search_all_tweets, query=query,
+                              tweet_fields=['context_annotations', 'created_at'],
+                              start_time=start_time,
+                              end_time=end_time,
+                              max_results=100).flatten(limit=10000):
+    print(tweet.data)
